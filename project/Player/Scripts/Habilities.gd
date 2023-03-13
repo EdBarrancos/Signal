075a6c28e7087c326649197dba014f2465355b1c
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-03-13 22:51:29 +0000

    Improve boost

--- project/Player/Scripts/Habilities.gd
+++ project/Player/Scripts/Habilities.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var movement_handler = $"../Movement"
 onready var player_animation = $"../Sprites/PlayerAnimation"
+onready var boost_sound = $"../Boost_Sound"
+onready var boost_particles = $"../BoostParticles"
 
 # Boost
 export(float) var BOOST_LENGTH = 0.2
@@ -19,15 +21,19 @@ func init(_parent):
 func activate_boost():
 	var boosting = not boostTimer.is_stopped()
 	
-	boostTimer.set_wait_time(boost_length)
-	boostTimer.one_shot = true
-	boostTimer.start()
-	
 	if not boosting:
+		boostTimer.set_wait_time(boost_length)
+		boostTimer.one_shot = true
+		boostTimer.start()
+	
 		movement_handler.set_turning(false)
 		movement_handler.boost(boost_speed)
 		
 		player_animation.play("boost_init")
+		
+		boost_sound.play_sound()
+		boost_particles.restart()
+		get_parent().set_collision_layer_bit(4, false)
 
 ##############
 #MISCELANIOUS#
@@ -37,3 +43,4 @@ func _on_BoostTimer_timeout():
 	movement_handler.set_turning(true)
 	player_animation.play("boost_end")
 	movement_handler.stop_boost(boost_speed)
+	get_parent().set_collision_layer_bit(4, true)
