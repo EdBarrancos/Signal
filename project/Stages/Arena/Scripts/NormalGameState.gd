27b8356c5d84cf30d0ddaef0669b6ddb63d3bf95
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-06-19 09:01:28 +0100

    Ramp up sheep spawing

--- project/Stages/Arena/Scripts/NormalGameState.gd
+++ project/Stages/Arena/Scripts/NormalGameState.gd
@@ -7,7 +7,13 @@ onready var temp_position = $Position2D
 
 onready var score_lives_label = $CanvasLayer
 
-export(int) var number_of_notes = 10;
+onready var spawn_interval_timer = $SpawnInterval
+
+export(int) var max_number_of_notes = 15;
+export(int) var starting_number_of_notes = 3;
+export(float) var spawning_interval = 5.0;
+export(float) var interval_increase = 0.5;
+var current_notes
 
 func _ready():
 	pass
@@ -49,7 +55,24 @@ func _on_CameraTransiction_finished():
 	score_lives_label.set_score(0)
 	score_lives_label.reset_lives()
 	spawner.spawn_patch().connect("eaten", self, "on_patch_eaten")
-	for _i in range(number_of_notes):
+	for _i in range(starting_number_of_notes):
 		var note = spawner.spawn_note_in_random_position()
 		note.connect("died", self, "on_note_killed")
 		note.connect("caught", self, "on_note_caught")
+	current_notes = starting_number_of_notes
+	spawn_interval_timer.set_wait_time(spawning_interval)
+	spawn_interval_timer.start()
+
+func _on_SpawnInterval_timeout():
+	if current_notes >= max_number_of_notes:
+		return
+	
+	var note = spawner.spawn_note_in_random_position()
+	note.connect("died", self, "on_note_killed")
+	note.connect("caught", self, "on_note_caught")
+	current_notes += 1
+	spawning_interval += interval_increase
+	spawn_interval_timer.set_wait_time(spawning_interval)
+	spawn_interval_timer.start()
+	
+	
