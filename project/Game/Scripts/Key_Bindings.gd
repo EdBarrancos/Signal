53fbb35e52ce2c1dde59d618f008d956b1dcf3aa
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-06-28 18:50:54 +0100

    Improve dynamic keyboard tutorial

--- project/Game/Scripts/Key_Bindings.gd
+++ project/Game/Scripts/Key_Bindings.gd
@@ -1,33 +1,42 @@
 extends Node2D
 
 onready var is_using_keyboard = true
+onready var is_arrows_right = true
 
 signal input_changed
 
 func _ready():
 	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
 	if Input.get_connected_joypads().size() >= 1:
-		SetController()
+		set_controller()
 
-func SetController():
+func set_controller():
 	is_using_keyboard = false
 	emit_signal("input_changed")
 
-func SetKeyboard():
+func set_keyboard():
 	is_using_keyboard = true
 	emit_signal("input_changed")
-	
+
+func set_keyboard_with_event(event):
+	if event.is_action_pressed("LEFT") or event.is_action_pressed("RIGHT"):
+		if event.scancode in [KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT]:
+			is_arrows_right = true
+		else:
+			is_arrows_right = false
+	set_keyboard()
+		
 func _on_joy_connection_changed(device_id, connected):
 	if connected:
-		SetController()
+		set_controller()
 	else:
-		SetKeyboard()
+		set_keyboard()
 		
 func _input(event):
 	if event is InputEventKey:
-		SetKeyboard()
+		set_keyboard_with_event(event)
 	elif event is InputEventJoypadButton:
-		SetController()
+		set_controller()
 
 func get_key_binding_pin():
 	return get_printable_keycode(get_active_input_event("CHORD"))
@@ -42,12 +51,22 @@ func get_printable_keycode(event: InputEvent):
 		return map_joystick_index_to_button(event.button_index)
 
 func get_active_input_event(event_name):
+	# Expected: Arrow right - Arrow left - Controller
+	# OR
+	# Keyboard - Controller
 	var events = InputMap.get_action_list(event_name)
-	for event in events:
-		if is_playing_with_keyboard() and event is InputEventKey:
-			return event
-		if !is_playing_with_keyboard() and event is InputEventJoypadButton:
-			return event
+	
+	if is_playing_with_keyboard():
+		if is_using_arrows_right():
+			return events[0]
+		else:
+			if events.size() == 3:
+				return events[1] 
+			return events[0]
+	else:
+		if events.size() == 3:
+			return events[2] 
+		return events[1]
 
 func map_joystick_index_to_button(index):
 	# TODO: This is only taking into account XBOX commands. Could be improved
@@ -63,3 +82,6 @@ func map_joystick_index_to_button(index):
 	
 func is_playing_with_keyboard():
 	return is_using_keyboard
+
+func is_using_arrows_right():
+	return is_arrows_right
