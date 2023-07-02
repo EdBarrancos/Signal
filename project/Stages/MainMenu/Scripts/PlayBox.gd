4a19c6a3beae15c3ed855bb09584e68103e5d11f
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-07-02 18:35:57 +0100

    remove Shader from fence

--- project/Stages/MainMenu/Scripts/PlayBox.gd
+++ project/Stages/MainMenu/Scripts/PlayBox.gd
@@ -1,7 +1,12 @@
 extends StaticBody2D
 
 export var max_wave_size : float;
-export var animation_time : float;
+export var wave_animation_time : float;
+onready var queue_wave_animation = null;
+
+export var flash_animation_time : float;
+onready var queue_flash_animation = null;
+
 
 onready var tween = $Tween
 onready var sprite = $Sprite
@@ -12,10 +17,6 @@ func _ready():
 func init(parent):
 	pass
 
-#func _process(delta):
-#   pass
-
-
 
 func _on_PlayerCollision_body_entered(body):
 	var waving = (self.global_position - body.global_position).normalized() * max_wave_size;
@@ -24,17 +25,32 @@ func _on_PlayerCollision_body_entered(body):
 		"shader_param/wind", 
 		Vector2.ZERO, 
 		waving, 
-		animation_time / 2, 
+		wave_animation_time / 2, 
 		Tween.TRANS_CUBIC,Tween.EASE_OUT)
-	tween.start()
-	
-	yield(tween,"tween_completed")
+		
 	tween.interpolate_property(
 		sprite.material, 
 		"shader_param/wind", 
 		waving, 
 		Vector2.ZERO,
-		animation_time / 2,
+		wave_animation_time / 2,
 		Tween.TRANS_ELASTIC, 
-		Tween.EASE_OUT)
+		Tween.EASE_OUT,
+		wave_animation_time / 2)
+	
+	tween.interpolate_property(
+		sprite.material, 
+		"shader_param/flash_modifier",
+		0,
+		1,
+		flash_animation_time / 2) 
+	tween.interpolate_property(
+		sprite.material, 
+		"shader_param/flash_modifier",
+		1,
+		0,
+		flash_animation_time / 2, 
+		Tween.TRANS_LINEAR, 
+		Tween.EASE_IN_OUT, 
+		flash_animation_time / 2)
 	tween.start()
