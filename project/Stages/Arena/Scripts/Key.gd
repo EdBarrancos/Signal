c2068d619e6ce3cd8aa2136057bff56d461eccff
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-07-05 20:22:12 +0100

    Update Tutorial

--- project/Stages/Arena/Scripts/Key.gd
+++ project/Stages/Arena/Scripts/Key.gd
@@ -5,8 +5,10 @@ export var input_expected : String
 export var arrow_keyboard_texture : Texture
 export var awsd_keyboard_texture : Texture
 
+onready var sprite = self
+
 func _ready():
-	pass
+	KeyBindings.connect("input_changed", self, "input_changed")
 
 func init(parent):
 	pass
@@ -16,3 +18,10 @@ func _process(delta):
 		self.frame = 1
 	else:
 		self.frame = 0
+
+func input_changed():
+	if KeyBindings.is_playing_with_keyboard():
+		if KeyBindings.is_using_arrows_right():
+			sprite.texture = arrow_keyboard_texture
+		else:
+			sprite.texture = awsd_keyboard_texture
